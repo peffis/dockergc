5a74047472e8304e04dd53770ef8d5a8cb65735b
Stefan Hellkvist <hellkvist@gmail.com>
2017-02-14 09:25:55 +0100

    adding missing quotes

--- dockergc.sh
+++ dockergc.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # set DOCKER to sudo docker if you need to run docker with root permissions
-DOCKER=docker
-#DOCKER=sudo docker
+DOCKER="docker"
+#DOCKER="sudo docker"
 
 # remove docker instances that have status=exited
 for c in $($DOCKER ps -a -q -f status=exited)
